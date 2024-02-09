2b4f9754495deb5a3640b75ba8bab7619ddd77d2
Roronoa-Z040 <135990307+Roronoa-Z040@users.noreply.github.com>
2024-02-09 18:05:00 +0530

    create and run server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
     <BrowserRouter>
     <Header />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp/>} />
         <Route path="/about" element={<About />} />
