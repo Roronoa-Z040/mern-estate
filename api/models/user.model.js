f039ac6212fb11f2fdcd37ee81a1b169d7aa2656
Roronoa-Z040 <135990307+Roronoa-Z040@users.noreply.github.com>
2024-02-10 13:13:42 +0530

    create sign up api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,7 +1,8 @@
 import mongoose  from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-    usernames: {
+const userSchema = new mongoose.Schema(
+    {
+    username: {
         type: String,
         required: true,
         unique: true,   
@@ -15,9 +16,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,   
     }
-},{timestamps: true});
+    },
+    {timestamps: true}
+);
 
-const User = new mongoose.model('User',userSchema);
+const User =  mongoose.model('User',userSchema);
 
 export default User;
 
