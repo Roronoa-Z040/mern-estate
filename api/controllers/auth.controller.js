520661fbf8eb824e3e40400431993a548865499d
Roronoa-Z040 <135990307+Roronoa-Z040@users.noreply.github.com>
2024-02-24 11:49:05 +0530

    Create user update API route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-import { erreoHandler } from "../utils/error.js";
+import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res, next) => {
@@ -13,7 +13,7 @@ export const signup = async (req,res, next) => {
         res.status(201).json('user created successfully!');
     }catch(error){
         next(error);
-       /*  next(erreoHandler(550, 'error from the function ')); */
+       /*  next(errorHandler(550, 'error from the function ')); */
     }
     
 };
@@ -22,9 +22,9 @@ export const signin = async(req, res, next) => {
     const{ email, password } = req.body;
     try {
         const validUser = await User.findOne({ email });
-        if (!validUser) return next(erreoHandler(404, 'User not found !'));
+        if (!validUser) return next(errorHandler(404, 'User not found !'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
-        if (!validPassword) return next(erreoHandler(401, 'Wrong credentials !'));
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials !'));
         const token = jwt.sign({ id : validUser._id }, process.env.JWT_SECRET)
         const { password: pass, ...rest } =validUser._doc;
         res
