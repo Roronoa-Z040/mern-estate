a06b55168a9b8764f2ad977cd3b601d104c334e9
Roronoa-Z040 <135990307+Roronoa-Z040@users.noreply.github.com>
2024-03-12 15:46:13 +0530

    Complete update listing functionality

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app }  from '../firebase';
 import { set } from 'mongoose';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 
 
 export default function CreateListing() {
     const {currentUser} = useSelector(state => state.user);
     const navigate = useNavigate();
+    const params = useParams();
     const [files, setFiles] = useState([]);
     const [formData, setFormData] = useState({
         imageUrls: [],   
@@ -29,8 +30,20 @@ export default function CreateListing() {
     const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
 
-    console.log(formData);
-    
+    useEffect(() => {
+        const fetchListing = async () => {
+            const listingId = params.listingId;
+            const res = await fetch(`/api/listing/get/${listingId}`);
+            const data = await res.json();
+            if (data.success === false) {
+                console.log(data.message);
+                return;
+            }  
+            setFormData(data);
+        };
+        fetchListing();
+    }, []);
+
     const handleImagesubmit = (e) => {
         if (files.length > 0 && files.length + formData.imageUrls.length < 7 ) {
             setUploading(true);
@@ -118,7 +131,7 @@ export default function CreateListing() {
             if(+formData.regularPrice < +formData.discountPrice) return setError('Discount price must be lower than regular price !');
             setLoading(true);
             setError(false);
-            const res = await fetch('/api/listing/create', {
+            const res = await fetch(`/api/listing/update/${params.listingId}`, {
                 method: 'POST',
                 headers: { 
                     'Content-Type': 'application/json' ,
@@ -143,7 +156,7 @@ export default function CreateListing() {
 
   return (
     <main className='p-3 max-w-4xl mx-auto'>
-        <h1 className='text-3xl font-bold text-center my-7' >Create a Listing</h1>
+        <h1 className='text-3xl font-bold text-center my-7' >Update Listing</h1>
         <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-4'>
             <div className='flex flex-col gap-4 flex-1'>
                 <input 
@@ -328,7 +341,7 @@ export default function CreateListing() {
                     ))
                  }
                 <button disabled={loading || uploading } className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>
-                    {loading ? 'Creating...' : 'Create Listing'}
+                    {loading ? 'Creating...' : 'Update  Listing'}
                 </button>
                 {error && <p className='text-red-700 text-sm'>{error}</p> }
             </div>
@@ -338,3 +351,4 @@ export default function CreateListing() {
      </main>
   )
 }
+
